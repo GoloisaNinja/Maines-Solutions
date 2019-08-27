c1322e631d7b824c57153ee6592829c1478d3d76
Golodhrim <holidayninjastaff@gmail.com>
2019-08-27 00:04:18 -0400

    results upgrade commit
    results page add and other various implementations

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-const mainSurveyButton = document.querySelector('#iniSurvey')
-const mainHidden = document.querySelector('#hiddenForm')
-mainHidden.style.visibility = 'hidden'
+$(function(){
+  if($('body').is('.surveyPage')){
+    const mainSurveyButton = document.querySelector('#iniSurvey')
+    const mainHidden = document.querySelector('#hiddenForm')
+    mainHidden.style.visibility = 'hidden'
 
-mainSurveyButton.addEventListener('click', function(e){
-  if (mainHidden.style.visibility === 'hidden') {
-      mainHidden.style.visibility = 'visible'
-  } else {
-      mainHidden.style.visibility = 'hidden'
+    mainSurveyButton.addEventListener('click', function(e){
+      if (mainHidden.style.visibility === 'hidden') {
+          mainHidden.style.visibility = 'visible'
+      } else {
+          mainHidden.style.visibility = 'hidden'
+      }
+    })
   }
+
 })
+
+// const mainSurveyButton = document.querySelector('#iniSurvey')
+// const mainHidden = document.querySelector('#hiddenForm')
+// mainHidden.style.visibility = 'hidden'
+//
+// mainSurveyButton.addEventListener('click', function(e){
+//   if (mainHidden.style.visibility === 'hidden') {
+//       mainHidden.style.visibility = 'visible'
+//   } else {
+//       mainHidden.style.visibility = 'hidden'
+//   }
+// })
